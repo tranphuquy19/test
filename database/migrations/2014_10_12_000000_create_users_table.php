f2043b9de7794f783e16cebfdc94915ae7ec8889
tranphuquy19 <tranphuquy19@gmail.com>
2018-09-26 15:59:21 +0700

    Make auth, custom users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->char('sex', 1)->nullable(); //m = male, f = female;
+            $table->date('birthday')->nullable();
+            $table->string('image_avatar')->nullable(); //name file image avatar in public.images
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
